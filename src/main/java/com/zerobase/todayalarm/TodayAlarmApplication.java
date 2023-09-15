eb896c7d8cb1920fef5a03143c5872f44d70133d
Woogie95 <choisw950928@gmail.com>
2023-09-15 19:57:48 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/todayalarm/TodayAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.todayalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayAlarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayAlarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/todayalarm/TodayAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.todayalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayAlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
